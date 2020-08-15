c8fcaf0223ee781d806d42a5166091e4cf4dfe63
duochen13 <duochen@umich.edu>
2020-08-14 23:05:26 -0400

    delete gameoflife

--- attendenceCheck/opencv_playground.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-import cv2
-
-# https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html#display-image
-
-img = cv2.imread('test.png', 0)
-cv2.imshow('window name', img)
-
-
