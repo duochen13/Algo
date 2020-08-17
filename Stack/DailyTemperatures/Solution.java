166fe12998302f1951e58db1fc9063d851bad0c2
duochen13 <duochen@umich.edu>
2020-08-17 17:53:49 -0400

    Stack: add dialy temperature

---
+++ Stack/DailyTemperatures/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        int[] res = new int[T.length];
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for (int i = 0; i < T.length; ++i) {
+            while (!stack.isEmpty() && T[i] > T[stack.peek()]) {
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] T = {73,74,75,71,69,72,76,73};
+        int[] res = s.dailyTemperatures(T);
+        for (int num : res) {
+            System.out.println(num);
+        }
+        
+    }
+}
+
+//   0 1  2  3  4  5  6  7
+/// 73,74,75,71,69,72,76,73
+//                     ^
+//  
+// stack: [2,3]
+// index:  4
+// res[index] = i - index = 6 - 5 = 1
