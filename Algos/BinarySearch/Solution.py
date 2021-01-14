66ce1436dd7cdfa4fbeb9a1afecd9c9245f8d6a4
duochen13 <duochen@umich.edu>
2021-01-14 01:25:56 -0500

    Algo: binary search

---
+++ Algos/BinarySearch/Solution.py
@@ -0,0 +1,18 @@
+
+
+"""
+OA from Citadel:
+input: 
+sorted nums: [1,2,3,4,5]
+target num: 8
+
+find susequence a[i], a[j], a[k] where i < j < k and sum(a[i], a[j], a[k]) <= target
+
+sol:
+    for i:....
+        l, r = i + 1, nums.szie() - 1
+        while (l < r)
+            bst
+
+cnt += (r - l), actually interesting
+"""
