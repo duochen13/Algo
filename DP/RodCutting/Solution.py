337314c93ef97b71142cc6bba6d0df72d472cb77
duochen13 <duochen@umich.edu>
2021-03-07 21:40:28 -0500

    DP: Rod Cutting modifed

--- DP/RodCutting/Solution.py
+++ DP/RodCutting/Solution.py
@@ -4,6 +4,10 @@ input: n: length of rod, price: a list of price with corresponding rod length
 output: max profit
 """
 
+# f(n): the max profit with the rod of length n
+# f(n) = p[x] + f(n - x)
+# f(0) = 0
+
 
 def rodCuttingBottomUp(n, price):
     assert n < len(price)
@@ -11,6 +15,8 @@ def rodCuttingBottomUp(n, price):
     for j in range(1, n + 1):
         for i in range(j):
             dp[j] = max(dp[j], dp[i] + dp[j - i])
+    print(price)
+    print(dp)
     return dp[-1]
 
 
@@ -33,11 +39,52 @@ def rodCuttingTopDown(n, price):
     return helper(n, price, memo)
 
 
-price = [0,1,5,8,9, 10,17,17,20,24,30]
-answe = [0,1,5,8,10,13,17,18,22,25,30]
+"""
+Greedy does not work
+ex: p = [0, 4, 7, ] corresponding length = [1,2,3]
+    d = [0, 2, 2.3]
+    n = 4
+based on greedy stragegy, it will cut l=3, n-l=1, total price = 7+0 = 7
+but 2 rods of length = 2, yields the profits 4 + 4 = 8
+"""
+def rodCuttingDensity(n, price):
+    pass
 
-for n in range(len(price)):
-    res = rodCuttingTopDown(n, price)
-    assert res == answe[n]
+
+"""
+The revenue associated with a solution is now the sum of the prices of the pieces minus the costs of making the cuts.
+"""
+# f(n) = p[x] + f(n - x) + cost
+def rodCuttingModified(n, price, cost=1):
+    dp = [x for x in price]
+    for i in range(1, n + 1):
+        for j in range(i):
+            print("dp[{}] = {}, dp[{}] - cost + dp[{}] = {}".format(i, dp[i], j, i - j, dp[i - j]))
+            dp[i] = max(dp[i], dp[j] - cost + dp[i - j])
+        print("===>dp[{}] = {}".format(i, dp[i]))
+    print("cost:{}, price:{}".format(cost, price))
+    print("dp:{}".format(dp))
+    return dp[-1]
 
 
+"""
+The modified version of rodCutting algo with actual results
+"""
+def rodCuttingResults(n, price):
+    dp = [[x, [i]] for i, x in enumerate(price)]
+    for i in range(1, n + 1):
+        for j in range(i):
+            lastProfit, lastCoins = dp[i - j]
+            if lastProfit + dp[j][0] > dp[i][0]:
+                dp[i][0] = lastProfit + dp[j][0]
+                dp[i][1] = lastCoins + [j]
+    pass
+
+price_0 = [0,1,5,8,9, 10,17,17,20,24,30]
+answe_0 = [0,1,5,8,10,13,17,18,22,25,30]
+
+#        0 1 2 3
+price = [0,4,6,              8]
+answe = [0,4,4+4-1,4+4+4-2]
+
+assert rodCuttingModified(3, price) == 10
